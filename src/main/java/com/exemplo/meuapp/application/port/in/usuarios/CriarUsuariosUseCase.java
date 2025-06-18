876bbcda5a4384d0b9d1efedf3c7e538b32bc05f
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2025-06-18 00:23:07 -0300

    melhoria

--- src/main/java/com/exemplo/meuapp/application/port/in/usuarios/CriarUsuariosUseCase.java
+++ src/main/java/com/exemplo/meuapp/application/port/in/usuarios/CriarUsuariosUseCase.java
@@ -1,6 +1,7 @@
 package com.exemplo.meuapp.application.port.in.usuarios;
 
 import com.exemplo.meuapp.application.port.out.UsuariosGateways;
+import com.exemplo.meuapp.domain.enums.UsuarioTipo;
 import com.exemplo.meuapp.domain.model.Usuarios;
 import com.exemplo.meuapp.presentation.dto.NovoPerfil;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,8 +19,15 @@ public class CriarUsuariosUseCase {
     public Usuarios criar(NovoPerfil usuarios) {
 
         Usuarios usuarios1 = Usuarios.builder().usuario(usuarios.usuario()).senha(usuarios.senha()).email(usuarios.email()).status(usuarios.status()).build();
-
+        usuarios1.setTipo(UsuarioTipo.VISITANTE);
         usuarios1.setCriadoEm(LocalDateTime.now());
         return usuariosGateways.save(usuarios1);
+    }
+
+    public Usuarios criar(Usuarios usuarios) {
+
+
+        usuarios.setCriadoEm(LocalDateTime.now());
+        return usuariosGateways.save(usuarios);
     }
 }
--- target/generated-sources/annotations/com/exemplo/meuapp/common/mapper/AnexoMapperImpl.java
+++ target/generated-sources/annotations/com/exemplo/meuapp/common/mapper/AnexoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-18T00:01:57-0300",
+    date = "2025-06-18T00:22:29-0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 @Component
